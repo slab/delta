17a5fe5ac347399cb5a4a03b4bda3c2af40be464
Jason Chen <jhchen7@gmail.com>
2014-08-27 23:20:55 -0700

    setup testing

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  // Define to control testing order
+  var tests = [
+    'test/unit.js',
+  ];
+
+  grunt.registerTask('coverage', function() {
+    grunt.util.spawn({
+      cmd: './node_modules/.bin/istanbul',
+      args: ['cover', './node_modules/.bin/_mocha'].concat(tests).concat(['--dir', '.coverage']),
+      opts: { stdio: 'inherit' }
+    }, this.async());
+  });
+
+  grunt.registerTask('test', function() {
+    grunt.util.spawn({
+      cmd: './node_modules/.bin/mocha',
+      args: tests,
+      opts: { stdio: 'inherit' }
+    }, this.async());
+  });
+};
