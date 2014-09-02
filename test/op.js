1d94cdb3cd18006e554ea7df01614563759eb8f3
Jason Chen <jhchen7@gmail.com>
2014-09-01 22:14:01 -0700

    add tests and fix bug

--- test/op.js
+++ test/op.js
@@ -4,46 +4,56 @@ var expect = require('chai').expect;
 
 describe('op', function() {
   describe('composeFormats', function() {
-    it('left is null', function() {
-
+    var format = { bold: true, color: 'red' };
+    it('left is undefined', function() {
+      expect(op.composeFormats(undefined, format)).to.deep.equal(format);
     });
 
-    it('right is null', function() {
-
+    it('right is undefined', function() {
+      expect(op.composeFormats(format, undefined)).to.deep.equal(format);
     });
 
-    it('both are null', function() {
-
+    it('both are undefined', function() {
+      expect(op.composeFormats(undefined, undefined)).to.deep.equal(undefined);
     });
 
     it('missing', function() {
-
+      expect(op.composeFormats(format, { italic: true })).to.deep.equal({
+        bold: true,
+        italic: true,
+        color: 'red'
+      });
     });
 
     it('overwrite', function() {
-
+      expect(op.composeFormats(format, { bold: false, color: 'blue' })).to.deep.equal({
+        bold: false,
+        color: 'blue'
+      });
     });
 
     it('remove', function() {
-
+      expect(op.composeFormats(format, { bold: null })).to.deep.equal({
+        color: 'red'
+      });
     });
   });
 
   describe('length', function() {
     it('delete', function() {
-
+      expect(op.length({ delete: 5 })).to.equal(5);
     });
 
     it('retain', function() {
-
+      expect(op.length({ retain: 2 })).to.equal(2);
     });
 
     it('insert text', function() {
-
+      expect(op.length({ insert: 'text' })).to.equal(4);
     });
 
     it('insert embed', function() {
-
+      expect(op.length({})).to.equal(1);
     });
   });
 
