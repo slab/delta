4770d05de7bc63dc117bdb5f7425586b38bb17d4
Jason Chen <jhchen7@gmail.com>
2014-09-02 13:45:10 -0700

    add iterator tests and fix implementation

--- test/op.js
+++ test/op.js
@@ -1,3 +1,4 @@
+var Delta = require('../lib/delta');
 var op = require('../lib/op');
 var expect = require('chai').expect;
 
@@ -58,6 +59,69 @@ describe('op', function() {
   });
 
   describe('iterator', function() {
+    beforeEach(function() {
+      this.delta = new Delta().insert('Hello', { bold: true }).retain(3).delete(-4).insert({ src: 'http://quilljs.com/' });
+    });
 
+    it('hasNext() true', function() {
+      var iter = op.iterator(this.delta.ops);
+      expect(iter.hasNext()).to.equal(true);
+    });
+
+    it('hasNext() false', function() {
+      var iter = op.iterator([]);
+      expect(iter.hasNext()).to.equal(false);
+    });
+
+    it('peekLength() offset === 0', function() {
+      var iter = op.iterator(this.delta.ops);
+      expect(iter.peekLength()).to.equal(5);
+      iter.next();
+      expect(iter.peekLength()).to.equal(3);
+      iter.next();
+      expect(iter.peekLength()).to.equal(4);
+    });
+
+    it('peekLength() offset > 0', function() {
+      var iter = op.iterator(this.delta.ops);
+      iter.next(2);
+      expect(iter.peekLength()).to.equal(5 - 2);
+    });
+
+    it('peekLength() no ops left', function() {
+      var iter = op.iterator([]);
+      expect(iter.peekLength()).to.equal(Infinity);
+    });
+
+    it('peekType()', function() {
+      var iter = op.iterator(this.delta.ops);
+      expect(iter.peekType()).to.equal('insert');
+      iter.next();
+      expect(iter.peekType()).to.equal('retain');
+      iter.next();
+      expect(iter.peekType()).to.equal('delete');
+      iter.next();
+      expect(iter.peekType()).to.equal('insert');
+      iter.next();
+      expect(iter.peekType()).to.equal('retain');
+    });
+
+    it('next()', function() {
+      var iter = op.iterator(this.delta.ops);
+      for (var i = 0; i < this.delta.ops.length; i += 1) {
+        expect(iter.next()).to.deep.equal(this.delta.ops[i]);
+      }
+      expect(iter.next()).to.deep.equal({ retain: Infinity });
+      // Rerun to test multiple past end next calls
+      expect(iter.next()).to.deep.equal({ retain: Infinity });
+    });
+
+    it('next(length)', function() {
+      var iter = op.iterator(this.delta.ops);
+      expect(iter.next(2)).to.deep.equal({ insert: 'He', formats: { bold: true }});
+      expect(iter.next(10)).to.deep.equal({ insert: 'llo', formats: { bold: true }});
+      expect(iter.next(1)).to.deep.equal({ retain: 1 });
+      expect(iter.next(2)).to.deep.equal({ retain: 2 });
+    });
   });
 });
