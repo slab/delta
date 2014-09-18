9a87ecaa0629b429a33032baae04036d750f7217
Jason Chen <jhchen7@gmail.com>
2014-09-17 22:07:20 -0700

    refactor

--- test/fuzzer.js
+++ test/fuzzer.js
@@ -2,7 +2,6 @@ var Delta = require('quill-delta');
 Delta.op = require('quill-delta/lib/op');
 var fuzzer = require('ot-fuzzer');
 var richType = require('../lib/rich-text');
-var _ = require('lodash');
 
 var FORMATS = {
   color: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', null],
@@ -26,31 +25,30 @@ var generateRandomFormat = function () {
 var generateRandomOp = function (snapshot) {
   var snapshot = new Delta(snapshot);
   var length = snapshot.ops.reduce(function(length, op) {
-    return op.insert ? op.insert.length : 1;
+    // Snapshot should only have inserts
+    return length + (op.insert ? op.insert.length : 1);
   }, 0);
 
+  var base = length > 100 ? 3 : 2; // Favor deleting on long documents
   var delta = new Delta();
-  var lastOp = { retain: 1 };
-  var change = 0;
-  for (var i = 0; i < length; ++i) {
-    var random = fuzzer.randomReal();
-    if (random < 0.15) {
-      lastOp = { retain: 1 };
-    } else if (random < 0.30) {
-      lastOp = { insert: fuzzer.randomWord() + ' ' };
-    } else if (random < 0.45 || (random < 0.9 && length + change > 100)) {
-      lastOp = { 'delete': 1 };
+  do {
+    var random = fuzzer.randomInt(base);
+    // Allows insert/delete to occur at the end (deletes will be noop)
+    var index = fuzzer.randomInt(Math.min(length, 5) + 1);
+    length -= index;
+    delta.retain(index);
+    if (random === 0) {
+      delta.insert(fuzzer.randomWord());
+    } else {
+      var deleteLength = Math.min(length, fuzzer.randomInt(4) + 1);
+      delta.delete(deleteLength);
+      length -= deleteLength;
     }
-    delta._push(_.cloneDeep(lastOp));
-    change += Delta.op.length(lastOp);
-  }
-
-  if (fuzzer.randomReal() < 0.35) {
-    delta.insert(fuzzer.randomWord());
-  }
+  } while (length > 0 && fuzzer.randomInt(4) > 0);
 
   var composed = snapshot.compose(delta);
   return [delta.ops, composed.ops];
 };
+
 
 fuzzer(richType, generateRandomOp);
